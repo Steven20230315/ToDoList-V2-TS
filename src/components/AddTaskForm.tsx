e7367fff71abd23671f284500d80aa2cc9e7df47
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-03-06 21:46:36 +0800

    refactor: Update AddTaskForm with new form components
    Refactor AddTaskForm to integrate  `Form` and `Input` components.This
    change shifts away from direct state management of individual input
    fields to a more consolidated form handling approach with useRef. It
    simplifies the component's logic by using a single handleSave function
    for form submission, making the code cleaner and more maintainable.
    
    - Currently, the submit button will not be disabled when the required
        fields are empty. I'll fix this later.

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -3,8 +3,10 @@
 import style from './css/AddTaskForm.module.css';
 import CancelBtn from './CancelBtn';
 import SubmitBtn from './SubmitBtn';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { type TaskInfo } from '../App';
+import Input from './Input';
+import Form, { FormHandler } from './Form';
 
 type AddTaskFormProps = {
 	onCancel: () => void;
@@ -12,74 +14,79 @@ type AddTaskFormProps = {
 };
 
 export default function AddTaskForm({ onCancel, onAddTask }: AddTaskFormProps) {
-	const [enteredTitle, setEnteredTitle] = useState('');
-	const [enteredDescription, setEnteredDescription] = useState('');
+	const formRef = useRef<FormHandler>(null);
 
 	const [isDataComplete, setIsDataComplete] = useState(false);
+	const handleSave = (value: unknown) => {
+		type FormData = {
+			title: string;
+			description: string;
+		};
 
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target;
-		if (name === 'title') {
-			setEnteredTitle(value);
-		}
-		if (name === 'description') {
-			setEnteredDescription(value);
-		}
-	};
-
-	// title and description are required. If they are empty, set isDataComplete to false which will disable the submit button
-	// Initially, I want to avoid using useState and onChange to control the input field. So I use useRef to access the value of the input field. But I want to disable the submit button when the input field is empty. This involved rendering the submit button conditionally with disabled property based on the input field. useRef can't trigger a re-render. Even combined with useEffect, it can't trigger a re-render. (A least for me)
-	// https://react.dev/reference/react/useRef React useRef documentation
-	useEffect(() => {
-		if (enteredTitle && enteredDescription) {
-			setIsDataComplete(true);
-		}
-	}, [enteredTitle, enteredDescription]);
-
-	const handleAddTask = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-
-		// TODO: add component for select priority, label, date
 		const defaultPriority = 'Medium';
 		const defaultLabel = 'Personal';
 		const defaultDate = new Date().toISOString().slice(0, 10);
 		const taskId = Math.random();
 
-		onAddTask({
-			title: enteredTitle,
-			description: enteredDescription,
-			date: defaultDate,
+		const data: TaskInfo = {
+			...(value as FormData),
 			priority: defaultPriority,
 			label: defaultLabel,
+			date: defaultDate,
 			id: taskId,
-		});
-		// Don't need to clear form here. Since when the form is submitted, AddTaskForm is unmounted.
+		};
+		formRef.current?.clear();
+		onAddTask(data);
 	};
 
+	// title and description are required. If they are empty, set isDataComplete to false which will disable the submit button
+	// Initially, I want to avoid using useState and onChange to control the input field. So I use useRef to access the value of the input field. But I want to disable the submit button when the input field is empty. This involved rendering the submit button conditionally with disabled property based on the input field. useRef can't trigger a re-render. Even combined with useEffect, it can't trigger a re-render. (A least for me)
+	// https://react.dev/reference/react/useRef React useRef documentation
+	// useEffect(() => {
+	// 	if (enteredTitle && enteredDescription) {
+	// 		setIsDataComplete(true);
+	// 	}
+	// }, [enteredTitle, enteredDescription]);
+
+	// const handleAddTask = (e: React.FormEvent<HTMLFormElement>) => {
+	// 	e.preventDefault();
+
+	// 	// TODO: add component for select priority, label, date
+	// 	const defaultPriority = 'Medium';
+	// 	const defaultLabel = 'Personal';
+	// 	const defaultDate = new Date().toISOString().slice(0, 10);
+	// 	const taskId = Math.random();
+
+	// 	onAddTask({
+	// 		title: enteredTitle,
+	// 		description: enteredDescription,
+	// 		date: defaultDate,
+	// 		priority: defaultPriority,
+	// 		label: defaultLabel,
+	// 		id: taskId,
+	// 	});
+	// 	// Don't need to clear form here. Since when the form is submitted, AddTaskForm is unmounted.
+	// };
+
 	return (
-		<form
+		<Form
 			autoComplete='off'
-			onSubmit={handleAddTask}
+			onSave={handleSave}
 			className={style.add_task_form_container}
 		>
 			<div className={style.add_task_form_header}>
 				<div className={style.add_task_form_input_container}>
-					<input
+					{/* <input
 						type='text'
 						name='title'
 						id='title'
 						placeholder='Task name'
 						value={enteredTitle}
 						onChange={handleInputChange}
-					/>
-					<input
-						type='text'
-						name='description'
-						id='description'
-						placeholder='Description'
-						value={enteredDescription}
-						onChange={handleInputChange}
-					/>
+					/> */}
+
+					<Input type='text' id='title' placeholder='Task name' />
+					<Input type='text' id='description' placeholder='Description' />
 				</div>
 				<div className={style.add_task_form_action_group}>
 					<div>Date</div>
@@ -90,8 +97,9 @@ export default function AddTaskForm({ onCancel, onAddTask }: AddTaskFormProps) {
 			<div className={style.add_task_form_btn_container}>
 				<CancelBtn onClick={onCancel} />
 
-				<SubmitBtn text='Add task' disabled={!isDataComplete} />
+				{/* <SubmitBtn text='Add task' disabled={!isDataComplete} /> */}
+				<SubmitBtn text='Add task' />
 			</div>
-		</form>
+		</Form>
 	);
 }
