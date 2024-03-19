489209639caa91793f8b6d6228917bdee171e4fd
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-03-19 22:04:27 +0800

    fix: add missing semicolon

--- src/components/DatePickerBtn.tsx
+++ src/components/DatePickerBtn.tsx
@@ -3,12 +3,12 @@ import SchedulerMenu from './SchedulerMenu';
 import style from './css/DatePickerBtn.module.css';
 
 export default function DataPickerBtn() {
-  const [open, setOpen] = useState(false);
-  
-  const handleClick = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setOpen(!open);
-  }
+	const [open, setOpen] = useState(false);
+
+	const handleClick = (e: React.MouseEvent) => {
+		e.preventDefault();
+		setOpen(!open);
+	};
 
 	return (
 		<button onClick={handleClick} className={style.date_picker_btn}>
